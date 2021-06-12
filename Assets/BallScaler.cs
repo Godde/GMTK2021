94f0434fd4d00af70f0d00d36c08bc9e24e5ef18
Godde <Godde@Me>
2021-06-12 22:14:44 +0200

    Ball scaler

---
+++ Assets/BallScaler.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScaler : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localScale = new Vector3(transform.position.y + 1, transform.position.y + 1, transform.position.y + 1);
+    }
+}
